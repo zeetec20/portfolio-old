d91a17fedc6833f28825d46173889b082bc40b57
Firman363 <jusles363@gmail.com>
2019-09-19 10:11:11 +0700

    test preload website

--- function.js
+++ function.js
@@ -1,3 +1,11 @@
+var key1 = 500, key2 = 1540;
+
+function titleMargin(params) {
+    if (mediaQuery(1100, 1500).matches) {
+        document.getElementById("titleHome").style.marginTop = "9.5%";
+    }
+}
+
 function navbarSpanVisible(id1, id2) {
     document.getElementById("span"+id1+id2).style.transition = "all 1s";
     document.getElementById("span"+id1+id2).style.visibility = "visible";
@@ -10,6 +18,29 @@ function navbarSpanVisible(id1, id2) {
 
 function navbarSpanHidden(id1, id2) {
     document.getElementById("span"+id1+id2).style.visibility = "hidden";
+}
+
+function titleVisible(no) {
+    switch (no) {
+        case 1:
+            document.getElementById("titleHome").style.marginTop = "9.5%";
+            break;
+        case 2:
+            document.getElementById("titleAbout").style.left = "12%";
+            break;
+        case 3:
+            // document.getElementById("titleHome").style.marginTop = "9.5%";
+            break;
+        case 4:
+            // document.getElementById("titleHome").style.marginTop = "9.5%";
+            break;
+        case 5:
+            // document.getElementById("titleHome").style.marginTop = "9.5%";
+            break;
+            
+        default:
+            break;
+    }
 }
 
 function navbarSpanHiddenAll(id) {
@@ -31,15 +62,69 @@ function navbarSpanHiddenAll(id) {
 
 function navbarSpanCheckScroll() {
     let scroll = document.documentElement.scrollTop;
-    if (scroll < 477) {
+    if (scroll < key1) {
         navbarSpanVisible(1, "a");
         navbarSpanVisible(1, "b");
         navbarSpanHiddenAll(1);
-    }else if (scroll > 477) {
+        titleVisible(1);
+    }else if (scroll < key2) {
         navbarSpanVisible(2, "a");
         navbarSpanVisible(2, "b");
         navbarSpanHiddenAll(2);
+        titleVisible(2);
     }else if (condition) {
         
+    }
+}
+
+function mediaQuery(min, max) {
+    let maxWidth = "(max-width: " + max + "px)";
+    let minWidth = "(min-width: " + min + "px)";
+    var mediaQuery = window.matchMedia(minWidth + " and " + maxWidth);
+    return mediaQuery;
+}
+
+function navbarClick(no) {
+    let key, destination;
+    switch (no) {
+        case 1:
+            key = key1;
+            destination = "0";
+            break;
+        
+        case 2:
+            key = key2;
+            destination = "803";
+            break;
+        case 3:
+            break;
+
+        case 4:
+
+            break;
+
+        case 5:
+
+            break;
+    }
+
+    window.scrollTo({top: destination, behavior: "smooth"})
+}
+
+function titleResetMargin(except) {
+    let title1 = document.getElementById("titleHome").style.marginTop = "-9.5%";
+
+    if (mediaQuery(1100, 1500).matches) {
+        switch (except) {
+            case 1:
+                
+                break;
+            case 2:
+                title1;
+                break;
+
+            default:
+                break;
+        }
     }
 }
--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
-
 var color = 800;
 
-window.addEventListener("scroll", function (event) {
+document.body.style.overflow = "hidden";
+
+
+window.addEventListener("load", function () {
+    document.getElementById('loading').style.display = "none";
+    document.body.style.overflow = "visible";
+})
+
+window.addEventListener("scroll", function () {
     var scroll = document.documentElement.scrollTop;
     console.log(scroll);
     // if (scroll >= 50) {
     //     color = scroll * 15;
     // }
     navbarSpanCheckScroll();
+    if (document.documentElement.scrollTop < key1) {
+        titleResetMargin[1];
+        document.getElementById("titleAbout").style.left = "-16%";
+    }
+    if (document.documentElement.scrollTop >= key1 && document.documentElement.scrollTop < key2) {
+        console.log('odoqidq');
+        document.getElementById("titleHome").style.marginTop = "-9.5%";
+    }
 });
+
+
+
+if (mediaQuery(1100, 1500).matches) {
+    document.getElementById("titleHome").style.marginTop = "9.5%";
+}
+
+navbarClick(1);
 
 navbarSpanCheckScroll();

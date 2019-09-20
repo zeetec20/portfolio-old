e779a93d207bbd609d7966cfb668963588f57392
Firman363 <jusles363@gmail.com>
2019-09-20 23:20:23 +0700

    preloader website selesai

--- preloader.js
+++ preloader.js
@@ -2,19 +2,28 @@
 var preloader1, preloader2, preloader;
 var timer = 0;
 
-document.body.style.overflow = "hidden";
-
 var timerPreloader = setInterval(() => { 
     timer += 1;
     if (timer >= 3) {
         preloader1 = true;
         window.preloader1;
         if (preloader2 == true) {
-            document.getElementById('loading').style.display = "none";
-            document.body.style.overflow = "visible";
             
             var script = document.createElement('script');
-            script.setAttribute('src','script.js');
+            script.src = "script.js";
+            var style = document.getElementById('style');
+            style.setAttribute('href', './asset/css/style.css');
+
+            // document.head.appendChild(style);
+            document.getElementById('loading').style.opacity = "0";
+
+            if (document.getElementById('loading').style.opacity == "0") {
+                var timerLoading = setInterval(() => {
+                    document.getElementById('loading').style.display = "none";
+                    clearInterval(timerLoading);
+                }, 900);
+            }
+            
             document.body.appendChild(script);
 
             clearInterval(timerPreloader);
